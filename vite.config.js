ba58c44b7d2b8143e2876a18682f516cf67c0f43
AbhiAbhi <abhishekmingal658@gmail.com>
2025-08-12 02:03:26 +0530

    Update repository name to LCG

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/pwa-test/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/LCG/' : '/',
   server: {
     host: '0.0.0.0',
     port: 5174,
